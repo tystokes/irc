298b3a574f822f9f47ccea8e1bf707f302b9daee
Tyler Stokes <tystokes@umich.edu>
2014-04-20 13:22:27 -0400

    Better auto-updating.

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ from subprocess import call
 # requires command line git to be installed
 # commits any changes made to the main and attempts to merge with master
 """
-autoCommit = call("git commit -am \"Auto commit main\".", shell=True)
-git = call("git pull origin master", shell=True)
-checkoutOurMain = call("git checkout --ours main.py", shell=True)
+autoCommit = call("git commit -am \"Auto commit main\"", shell=True)
+print("autoCommit return code : " + str(autoCommit))
+gitPull = call("git pull origin master", shell=True)
+print("gitPull return code : " + str(gitPull))
+if gitPull != 0:
+	checkoutTheirs = call("git checkout --theirs .*", shell=True)
+	print("checkoutTheirs return code : " + str(checkoutTheirs))
+	checkoutOurMain = call("git checkout --ours main.py", shell=True)
+	print("checkoutOurMain return code : " + str(checkoutOurMain))
 """
 
 # IRCConnection(network, port, nick)
